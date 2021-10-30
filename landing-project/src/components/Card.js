dcaca2a7a4d39eb60f3ab23c82b47c76d2b56f29
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-10-31 02:45:15 +0330

    arrows and number of products added to my card & search bar edited

--- landing-project/src/components/Card.js
+++ landing-project/src/components/Card.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import style from "./Card.module.css"
+import down from "../images/down.svg"
+import up from "../images/up.svg"
 
 class Card extends Component {
+    constructor(){
+        super()
+        this.state ={
+            counter : 0
+        }
+    }
+    upHandler =()=>{
+        this.setState(prevState =>({
+            counter:prevState.counter +1
+        }))
+    }
+    downHandler =()=>{
+        if (this.state.counter>=1) {
+            this.setState(prevState =>({
+                counter:prevState.counter -1
+            }))
+        }
+      
+    }
     render() {
         const {image,name,cost} = this.props;
+        const {counter} =this.state
         return (
             <div className={style.container}>
-                <img src={image} alt="Phones" />
+                <img src={image} alt="Phones"  />
                 <h3>{name}</h3>
                 <p>{cost}</p>
+                <div className={style.counter}>
+                  <img src={up} alt="arrow" onClick={this.upHandler}/>
+                  <span>{counter}</span>
+                  <img className={this.state.counter ? "":style.deactive} src={down} alt="arrow" onClick={this.downHandler}/>  
+                </div>
             </div>
         );
     }
--- landing-project/src/components/Search.js
+++ landing-project/src/components/Search.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import styles from "./Search.module.css"
 
 class Search extends Component {
+    constructor(){
+        super()
+        this.state={
+            text:""
+        }
+    }
+    changeHandler = (e) =>{
+        this.setState({
+            text:e.target.value
+        })
+    }
     render() {
         return (
             <div className={styles.container}>
+                
                 <p>Search what you want</p>
-                <input placeholder="search..."/>
+                <div>
+                <input type="text" value={this.state.text} onChange={this.changeHandler} placeholder="search..."/>
+                <br/>
+                <br/>
+                <span>{this.state.text}</span>
+                </div>
             </div>
         );
     }
