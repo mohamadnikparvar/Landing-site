9105baf9d3d3d93367d40136f81bf94e5165b5eb
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-10-31 12:46:31 +0330

    cost and number of multiple orders of products added to my project and card component optimized

--- landing-project/src/components/Card.js
+++ landing-project/src/components/Card.js
@@ -30,11 +30,11 @@ class Card extends Component {
             <div className={style.container}>
                 <img src={image} alt="Phones"  />
                 <h3>{name}</h3>
-                <p>{cost}</p>
+                <p>{cost}{counter ? `* ${counter} = ${counter * Number(cost.split(" ")[0])}$`: ""}</p>
                 <div className={style.counter}>
                   <img src={up} alt="arrow" onClick={this.upHandler}/>
                   <span>{counter}</span>
-                  <img className={this.state.counter ? "":style.deactive} src={down} alt="arrow" onClick={this.downHandler}/>  
+                  <img className={!counter && style.deactive} src={down} alt="arrow" onClick={this.downHandler}/>  
                 </div>
             </div>
         );
--- landing-project/src/components/Cards.js
+++ landing-project/src/components/Cards.js
@@ -11,10 +11,10 @@ class Cards extends Component {
     render() {
         return (
             <div className={styles.container}>
-                <Card image={iphone13} name="Ipod pro" cost="500$"/>
-                <Card image={note20} name="Note 20" cost="400$"/>
-                <Card image={fold} name="Z-Fold" cost="450$"/>
-                <Card image={xiaomi9} name="Xiaomi 9" cost="300$"/>
+                <Card image={iphone13} name="Ipod pro" cost="500 $"/>
+                <Card image={note20} name="Note 20" cost="400 $"/>
+                <Card image={fold} name="Z-Fold" cost="450 $"/>
+                <Card image={xiaomi9} name="Xiaomi 9" cost="300 $"/>
             </div>
         );
     }
