bf016bbe058fb77b40282507b924de5fc050e88b
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-10-31 14:36:51 +0330

    cards components codes optimized

--- landing-project/src/components/Cards.js
+++ landing-project/src/components/Cards.js
@@ -8,13 +8,25 @@ import fold from "../images/zfold3.webp"
 import xiaomi9 from "../images/xiaomi9.jpg"
 
 class Cards extends Component {
+    constructor(){
+        super()
+        this.state ={
+            phoneData :[
+                {image:iphone13 , name:"Ipod pro", cost:"500 $"},
+                {image:note20, name:"Note 20", cost:"400 $"},
+                {image:fold, name:"Z-Fold", cost:"450 $"},
+                {image:xiaomi9, name:"Xiaomi 9", cost:"300 $"}
+            ]
+        }
+    }
     render() {
         return (
             <div className={styles.container}>
-                <Card image={iphone13} name="Ipod pro" cost="500 $"/>
+                {/* <Card image={iphone13} name="Ipod pro" cost="500 $"/>
                 <Card image={note20} name="Note 20" cost="400 $"/>
                 <Card image={fold} name="Z-Fold" cost="450 $"/>
-                <Card image={xiaomi9} name="Xiaomi 9" cost="300 $"/>
+                <Card image={xiaomi9} name="Xiaomi 9" cost="300 $"/> */}
+                {this.state.phoneData.map(phone =><Card key={phone.name} image={phone.image} name={phone.name} cost={phone.cost}/>)}
             </div>
         );
     }
